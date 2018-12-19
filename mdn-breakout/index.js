d830bba979b968109c0246051d028b724a9cdbd9
Carolina Melo <cmpm@cin.ufpe.br>
2018-12-19 15:33:49 -0300

    Implemented Paddle+movement

--- mdn-breakout/index.js
+++ mdn-breakout/index.js
@@ -1,10 +1,37 @@
 const canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d"); // actual tool to paint on canvas
-const ballRadius = 10;
+
+const ballRadius = 10,
+  paddleHeight = 10,
+  paddleWidth = 75;
+
 let x = canvas.width / 2,
   y = canvas.height - 30,
   dx = 2,
-  dy = -2;
+  dy = -2,
+  paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false,
+  leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -14,9 +41,18 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   // collision detection
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -25,6 +61,12 @@ function draw() {
 
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy *= -1;
+  }
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
 
   x += dx;
